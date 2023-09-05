8fee78d6637192655f0a4f701705da159a71f7c3
c emery <beecroncher@gmail.com>
2023-09-05 13:46:50 -0500

    created level object

---
+++ level.py
@@ -0,0 +1,34 @@
+from sense_hat import SenseHat
+
+class Level:
+	levelnum = 1
+	hole = [255,0,0]
+	wall = [0,0,0]
+	goal = [255,0,255]
+	levelarr = []
+	sense = SenseHat()
+
+	def __init__():
+		levelarr = loadLevelToArr("/levels/level1.png")
+		print(levelarr)
+
+	def isHole(x, y):
+		return levelarr[y*8+x] == hole
+	def isWall(x, y):
+		return levelarr[y*8+x] == wall
+	def isGoal(x, y):
+		return levelarr[y*8+x] == goal
+	def spawnCoords():
+		for i in range(64):
+			if levelarr[i] == [0,0,255]:
+				return [i%8, int(i/8)]
+
+	def nextLevel():
+		levelnum += 1
+		levelarr = loadLevelToArr("/levels/level"+level+".png")
+	def restart():
+		levelnum = 1
+		levelarr = loadLevelToArr("/levels/level1.png")
+
+	def loadLevelToArr(file):
+		return sense.load_image(file)
